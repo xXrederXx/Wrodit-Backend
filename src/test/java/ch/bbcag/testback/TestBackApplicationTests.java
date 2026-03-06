764dde5bc71ba4fb46ff8a382c1bf8c317864c1a
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-06 15:44:12 +0100

    feat(basic): add basic project

---
+++ src/test/java/ch/bbcag/testback/TestBackApplicationTests.java
@@ -0,0 +1,11 @@
+package ch.bbcag.testback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBackApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
