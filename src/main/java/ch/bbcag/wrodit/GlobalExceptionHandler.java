d033d4e612d856b3684ac52f68f21e6284b2e10b
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-11 15:25:28 +0100

    feat(error): handle the general exeptions with 500

---
+++ src/main/java/ch/bbcag/wrodit/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package ch.bbcag.wrodit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception ex) {
+        logger.error("An error occurred: {}", ex.getMessage(), ex);
+        return new ResponseEntity<>("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
