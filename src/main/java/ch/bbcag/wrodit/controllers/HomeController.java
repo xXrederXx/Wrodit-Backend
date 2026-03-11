d89ec1c05b649bf29ebfb8898a3a2f2ec7f670f2
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-11 12:48:15 +0100

    fix(HomeController): add request mapping

--- src/main/java/ch/bbcag/wrodit/controllers/HomeController.java
+++ src/main/java/ch/bbcag/wrodit/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package ch.bbcag.wrodit.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/home")
 public class HomeController {
 
   @GetMapping("/")
