5269f39222cc4b9d03a13577a275fd2af0e01269
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-11 10:35:55 +0100

    feat(HomeController): Add a hello world endpoint

---
+++ src/main/java/ch/bbcag/wrodit/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package ch.bbcag.wrodit.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "\"Hello World!\"";
+    }
+
+}
