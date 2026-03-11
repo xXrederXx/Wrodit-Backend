87a4442f88698dac0572bf04ac489041202c8ba8
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-11 10:39:28 +0100

    feat(entites): Add  Repositorys

---
+++ src/main/java/ch/bbcag/wrodit/repos/PostRepository.java
@@ -0,0 +1,8 @@
+package ch.bbcag.wrodit.repos;
+
+import ch.bbcag.wrodit.entitys.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
+}
---
+++ src/main/java/ch/bbcag/wrodit/repos/UserRepository.java
@@ -0,0 +1,8 @@
+package ch.bbcag.wrodit.repos;
+
+import ch.bbcag.wrodit.entitys.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
