35c280db02952b5d708262958ab28b13268bf59d
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-11 11:11:56 +0100

    fix(test): add custom test properties to use in memory db

--- src/test/java/ch/bbcag/wrodit/TestBackApplicationTests.java
+++ src/test/java/ch/bbcag/wrodit/TestBackApplicationTests.java
@@ -3,7 +3,7 @@ package ch.bbcag.wrodit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.name=application-test")
 class TestBackApplicationTests {
 
   @Test
