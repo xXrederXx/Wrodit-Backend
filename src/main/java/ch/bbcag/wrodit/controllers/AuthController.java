7c9cfa56316ea33f90b4bee6440da60547c1cc98
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-12 11:00:55 +0100

    feat(controller): add auth controller with /signin endpoint

---
+++ src/main/java/ch/bbcag/wrodit/controllers/AuthController.java
@@ -0,0 +1,47 @@
+package ch.bbcag.wrodit.controllers;
+
+import ch.bbcag.wrodit.dto.request.AuthRequestDTO;
+import ch.bbcag.wrodit.dto.response.AuthResponseDTO;
+import ch.bbcag.wrodit.entitys.User;
+import ch.bbcag.wrodit.mapper.AuthMapper;
+import ch.bbcag.wrodit.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(AuthController.PATH)
+public class AuthController {
+    public static final String PATH = "/auth";
+    private final UserService service;
+
+    public AuthController(UserService service) {
+        this.service = service;
+    }
+
+
+    @PostMapping("/signup")
+    @Operation(summary = "Create a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User was created successfully",
+                    content = @Content(schema = @Schema(implementation = AuthResponseDTO.class))),
+            @ApiResponse(responseCode = "409", description = "User could not be created, username already in use",
+                    content = @Content)
+    })
+    public ResponseEntity<?> signUp(
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "The user to register")
+            @Valid @RequestBody  AuthRequestDTO dto)
+    {
+        User auth = AuthMapper.fromDTO(dto);
+        User savedAuth = service.insert(auth);
+        return ResponseEntity.status(201).body(AuthMapper.toDTO(savedAuth));
+    }
+}
