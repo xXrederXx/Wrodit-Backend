afd4bcf997f2965090dc67370f89cb535b384bcb
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-12 15:17:11 +0100

    fix(cors): use cors in filter chain

--- src/main/java/ch/bbcag/wrodit/security/SecurityConfig.java
+++ src/main/java/ch/bbcag/wrodit/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,7 +19,9 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.csrf().disable().authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+    http.csrf(AbstractHttpConfigurer::disable)
+        .cors(cors -> {})
+        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
     return http.build();
   }
