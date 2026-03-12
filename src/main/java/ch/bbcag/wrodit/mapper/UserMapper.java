18ba39650ea9952834cf8278ff8bfa52012f77c4
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-12 12:41:17 +0100

    feat(mapping): add usermapper

---
+++ src/main/java/ch/bbcag/wrodit/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package ch.bbcag.wrodit.mapper;
+
+import ch.bbcag.wrodit.dto.response.UserResponseDTO;
+import ch.bbcag.wrodit.entitys.User;
+
+public class UserMapper {
+  private UserMapper() { // hide ctor
+  }
+
+  public static UserResponseDTO toDto(User user, boolean includeEmail) {
+    return new UserResponseDTO(
+        user.getId(), user.getUsername(), includeEmail ? user.getEmail() : "");
+  }
+}
