5651b0555d6145af4a5ce1765fc95c9b016fd951
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-12 07:36:00 +0100

    refactor(spotless): spotlessApply

--- src/main/java/ch/bbcag/wrodit/util/ErrorResponseDTO.java
+++ src/main/java/ch/bbcag/wrodit/util/ErrorResponseDTO.java
@@ -2,4 +2,4 @@ package ch.bbcag.wrodit.util;
 
 import java.time.LocalDateTime;
 
-public record ErrorResponseDTO<T>(T body, LocalDateTime timestamp){}
+public record ErrorResponseDTO<T>(T body, LocalDateTime timestamp) {}
