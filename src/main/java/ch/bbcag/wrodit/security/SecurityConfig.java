799dff79b0a6a461be72cecf6ddb1dafca625b21
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-12 14:52:53 +0100

    fix(cors): add cors config

--- src/main/java/ch/bbcag/wrodit/security/SecurityConfig.java
+++ src/main/java/ch/bbcag/wrodit/security/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -21,5 +26,16 @@ public class SecurityConfig {
     http.csrf().disable().authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
     return http.build();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("*"));
+    configuration.setAllowedMethods(List.of("*"));
+    configuration.setAllowedHeaders(List.of("*"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
