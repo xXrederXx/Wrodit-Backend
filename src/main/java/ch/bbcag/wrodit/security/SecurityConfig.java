913c50e39fb9558c973811bf56d42db31eea23a6
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-12 14:54:33 +0100

    refactor(spotless): spotlessApply

--- src/main/java/ch/bbcag/wrodit/security/SecurityConfig.java
+++ src/main/java/ch/bbcag/wrodit/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package ch.bbcag.wrodit.security;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,8 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
