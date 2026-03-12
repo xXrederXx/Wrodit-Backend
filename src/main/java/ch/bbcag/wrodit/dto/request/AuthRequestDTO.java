2da38b950102a4264056b875d8ef0d6241c284b5
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-12 14:03:58 +0100

    feat(security): add password constraints

--- src/main/java/ch/bbcag/wrodit/dto/request/AuthRequestDTO.java
+++ src/main/java/ch/bbcag/wrodit/dto/request/AuthRequestDTO.java
@@ -1,6 +1,7 @@
 package ch.bbcag.wrodit.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record AuthRequestDTO(
@@ -11,5 +12,9 @@ public record AuthRequestDTO(
         @Size(max = 255, message = "Email length must be not more than 255")
         String email,
     @NotBlank(message = "Password must not be empty")
-        @Size(max = 255, message = "Password length must be not more than 255")
+        @Size(min = 8, max = 255, message = "Password length must be not more than 255")
+        @Pattern(regexp = ".*[a-z].*", message = "Password must contain a lower case letter")
+        @Pattern(regexp = ".*[A-Z].*", message = "Password must contain a upper case letter")
+        @Pattern(regexp = ".*\\d.*", message = "Password must contain a number")
+        @Pattern(regexp = ".*\\W.*", message = "Password must contain a symbol")
         String password) {}
