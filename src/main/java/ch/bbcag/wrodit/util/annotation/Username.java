fbda3c0130bac7fefc212b4a80997ef241b1eec2
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-13 11:12:58 +0100

    fix(annotation): fix annotations with gpt

--- src/main/java/ch/bbcag/wrodit/util/annotation/Username.java
+++ src/main/java/ch/bbcag/wrodit/util/annotation/Username.java
@@ -3,6 +3,7 @@ package ch.bbcag.wrodit.util.annotation;
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import jakarta.validation.Constraint;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.lang.annotation.Retention;
@@ -10,7 +11,14 @@ import java.lang.annotation.Target;
 
 @Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
 @Retention(RUNTIME)
+@Constraint(validatedBy = {})
 // Java annotations
 @NotBlank(message = "Username must not be empty")
 @Size(max = 50, message = "Username length must be not more than 50")
-public @interface Username {}
+public @interface Username {
+    String message() default "Invalid username";
+
+    Class<?>[] groups() default {};
+
+    Class<?>[] payload() default {};
+}
