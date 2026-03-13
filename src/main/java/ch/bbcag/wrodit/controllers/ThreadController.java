1346db7301366c0a74ae3c92ffd4f85ba596aa1b
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-13 10:50:14 +0100

    feat(controller): add paginated threads endpoints

--- src/main/java/ch/bbcag/wrodit/controllers/ThreadController.java
+++ src/main/java/ch/bbcag/wrodit/controllers/ThreadController.java
@@ -1,7 +1,6 @@
 package ch.bbcag.wrodit.controllers;
 
 import ch.bbcag.wrodit.dto.response.ThreadResponseDTO;
-import ch.bbcag.wrodit.dto.response.UserResponseDTO;
 import ch.bbcag.wrodit.mapper.ThreadMapper;
 import ch.bbcag.wrodit.services.ThreadService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,33 +8,48 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(ThreadController.PATH)
 public class ThreadController {
-    public static final String PATH = "/threads";
-    private final ThreadService service;
+  public static final String PATH = "/threads";
+  private final ThreadService service;
 
-    public ThreadController(ThreadService service) {
-        this.service = service;
-    }
+  public ThreadController(ThreadService service) {
+    this.service = service;
+  }
 
-    @GetMapping("/{id}")
-    @Operation(summary = "Get a thread")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "Thread found",
-                            content = @Content(schema = @Schema(implementation = ThreadResponseDTO.class))),
-                    @ApiResponse(responseCode = "404", description = "Thread was not found", content = @Content)
-            })
-    public ResponseEntity<?> getById(@PathVariable Integer id) {
-        return ResponseEntity.ok(ThreadMapper.toDTO(service.findById(id)));
-    }
+  @GetMapping("/{id}")
+  @Operation(summary = "Get a thread")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Thread found",
+            content = @Content(schema = @Schema(implementation = ThreadResponseDTO.class))),
+        @ApiResponse(responseCode = "404", description = "Thread was not found", content = @Content)
+      })
+  public ResponseEntity<?> getById(@PathVariable Integer id) {
+    return ResponseEntity.ok(ThreadMapper.toDTO(service.findById(id)));
+  }
+
+  @GetMapping("/")
+  @Operation(summary = "Get multiple Threads")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Page generated",
+            content = @Content(schema = @Schema(implementation = Page.class))),
+      })
+  public ResponseEntity<?> getAllThreads(
+      @RequestParam(required = false, defaultValue = "0") Integer page,
+      @RequestParam(required = false, defaultValue = "20") Integer pageSize) {
+    return ResponseEntity.ok(
+        service.paginatedThreads(page, pageSize, Sort.unsorted()).map(ThreadMapper::toDTO));
+  }
 }
