044aec3737dd2fafa7c84c5b6e8e5d0a35b8ab5e
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-13 15:23:58 +0100

    refactor(security): extract header names

--- src/main/java/ch/bbcag/wrodit/security/SecurityConstants.java
+++ src/main/java/ch/bbcag/wrodit/security/SecurityConstants.java
@@ -2,4 +2,7 @@ package ch.bbcag.wrodit.security;
 
 public class SecurityConstants {
   public static final String AUTHORIZATION_HEADER_NAME = "Authorization";
+
+  public static final String AUTH_HEADER_PASSWORD = "WI-Auth-Passwd";
+  public static final String AUTH_HEADER_ID = "WI-Auth-Id";
 }
