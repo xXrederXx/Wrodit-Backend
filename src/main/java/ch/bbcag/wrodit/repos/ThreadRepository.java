bb7f14c1eb5c46444b1d70034221503ab3f4db75
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-13 15:25:36 +0100

    feat(repo): add find with user

--- src/main/java/ch/bbcag/wrodit/repos/ThreadRepository.java
+++ src/main/java/ch/bbcag/wrodit/repos/ThreadRepository.java
@@ -1,6 +1,13 @@
 package ch.bbcag.wrodit.repos;
 
 import ch.bbcag.wrodit.entitys.Thread;
+import ch.bbcag.wrodit.entitys.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ThreadRepository extends JpaRepository<Thread, Integer> {}
+import java.util.Set;
+
+public interface ThreadRepository extends JpaRepository<Thread, Integer> {
+    Page<Thread> findAllByUsersThreadUsersContaining(Set<User> usersThreadUsers, Pageable pageable);
+}
