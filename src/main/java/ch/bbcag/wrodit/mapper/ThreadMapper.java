06a5dc3bbfac09b748092bb3f19d0b7c44761c2f
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-18 15:53:39 +0100

    feat(mapper): add mapping fromDto

--- src/main/java/ch/bbcag/wrodit/mapper/ThreadMapper.java
+++ src/main/java/ch/bbcag/wrodit/mapper/ThreadMapper.java
@@ -1,11 +1,19 @@
 package ch.bbcag.wrodit.mapper;
 
+import ch.bbcag.wrodit.dto.request.ThreadRequestDTO;
 import ch.bbcag.wrodit.dto.response.ThreadPageResponseDTO;
 import ch.bbcag.wrodit.dto.response.ThreadResponseDTO;
 import ch.bbcag.wrodit.entitys.Thread;
 import org.springframework.data.domain.Page;
 
 public class ThreadMapper {
+  public static Thread fromDto(ThreadRequestDTO dto) {
+    Thread thread = new Thread();
+    thread.setName(dto.name());
+    thread.setDescription(dto.description());
+    return thread;
+  }
+
   public static ThreadResponseDTO toDTO(Thread therad) {
     return new ThreadResponseDTO(
         therad.getId(), therad.getName(), therad.getDescription(), therad.getCreatedAt());
