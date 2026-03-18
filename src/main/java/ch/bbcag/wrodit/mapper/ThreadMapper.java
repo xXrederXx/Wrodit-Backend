a3838ad87465272eb7e82651cee624a64e473eff
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-18 09:49:22 +0100

    feat(mapper): add a mapping for the pagable thread dto

--- src/main/java/ch/bbcag/wrodit/mapper/ThreadMapper.java
+++ src/main/java/ch/bbcag/wrodit/mapper/ThreadMapper.java
@@ -1,11 +1,19 @@
 package ch.bbcag.wrodit.mapper;
 
+import ch.bbcag.wrodit.dto.response.ThreadPageResponseDTO;
 import ch.bbcag.wrodit.dto.response.ThreadResponseDTO;
 import ch.bbcag.wrodit.entitys.Thread;
+import org.springframework.data.domain.Page;
 
 public class ThreadMapper {
   public static ThreadResponseDTO toDTO(Thread therad) {
     return new ThreadResponseDTO(
         therad.getId(), therad.getName(), therad.getDescription(), therad.getCreatedAt());
+  }
+
+  public static ThreadPageResponseDTO toPageDto(Page<Thread> page) {
+    ThreadPageResponseDTO dto = PageMapper.toDto(page, new ThreadPageResponseDTO());
+    dto.setContent(page.getContent().stream().map(ThreadMapper::toDTO).toList());
+    return dto;
   }
 }
