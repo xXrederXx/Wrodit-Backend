3231faf42d6efe370595daaf09cdecca6e758596
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-18 09:52:11 +0100

    refactor(spotless): spotlessApply

--- src/main/java/ch/bbcag/wrodit/controllers/ThreadController.java
+++ src/main/java/ch/bbcag/wrodit/controllers/ThreadController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
