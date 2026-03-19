8cade331b501e2bab32273ea20159f5ffa9e8f49
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-19 15:54:47 +0100

    refactor(spotless): spotlessApply

--- src/main/java/ch/bbcag/wrodit/security/KeySpecGenerator.java
+++ src/main/java/ch/bbcag/wrodit/security/KeySpecGenerator.java
@@ -1,31 +1,28 @@
 package ch.bbcag.wrodit.security;
 
 import jakarta.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
+import javax.crypto.spec.SecretKeySpec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-
 @Component
 public class KeySpecGenerator {
-    public static String SECRET;
-    public static final String ALGORITHM = "HmacSHA256";
+  public static String SECRET;
+  public static final String ALGORITHM = "HmacSHA256";
 
-    @Value("${jwt.secret}")
-    private String secret;
+  @Value("${jwt.secret}")
+  private String secret;
 
-    @PostConstruct
-    public void init() {
-        if (secret == null || secret.length() < 32) {
-            throw new RuntimeException("JWT secret too short or not set");
-        }
-        SECRET = secret;
+  @PostConstruct
+  public void init() {
+    if (secret == null || secret.length() < 32) {
+      throw new RuntimeException("JWT secret too short or not set");
     }
+    SECRET = secret;
+  }
 
-    public static SecretKeySpec getSecretKeySpec()
-    {
-        return new SecretKeySpec(SECRET.getBytes(StandardCharsets.UTF_8), ALGORITHM);
-    }
+  public static SecretKeySpec getSecretKeySpec() {
+    return new SecretKeySpec(SECRET.getBytes(StandardCharsets.UTF_8), ALGORITHM);
+  }
 }
-
