eca71251596d54a2cbe34b531721b790cd618267
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-19 11:52:00 +0100

    feat(jwt): add a user service

---
+++ src/main/java/ch/bbcag/wrodit/security/WroditUserDetailsService.java
@@ -0,0 +1,28 @@
+package ch.bbcag.wrodit.security;
+
+import ch.bbcag.wrodit.entitys.User;
+import ch.bbcag.wrodit.repos.UserRepository;
+import java.util.Collections;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WroditUserDetailsService implements UserDetailsService {
+  private final UserRepository userRepository;
+
+  public WroditUserDetailsService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user =
+        userRepository
+            .findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException(username));
+    return new org.springframework.security.core.userdetails.User(
+        user.getUsername(), user.getPasswordHash(), Collections.emptyList());
+  }
+}
