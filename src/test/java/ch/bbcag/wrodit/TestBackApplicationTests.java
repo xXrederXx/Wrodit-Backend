5951c71530cd90b83b614958659cb5acbc57376f
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-19 15:07:39 +0100

    refactor(spotless): spotlessApply

--- src/test/java/ch/bbcag/wrodit/TestBackApplicationTests.java
+++ src/test/java/ch/bbcag/wrodit/TestBackApplicationTests.java
@@ -3,7 +3,8 @@ package ch.bbcag.wrodit;
 import org.junit.jupiter.api.Test;
 import org.junitpioneer.jupiter.SetEnvironmentVariable;
 import org.springframework.boot.test.context.SpringBootTest;
-@SetEnvironmentVariable(key="JWT_SECRET", value = "Test-JWT-Secret-ONLY-4-TESTING-PURPOSE")
+
+@SetEnvironmentVariable(key = "JWT_SECRET", value = "Test-JWT-Secret-ONLY-4-TESTING-PURPOSE")
 @SpringBootTest(properties = "spring.config.name=application-test")
 class TestBackApplicationTests {
 
