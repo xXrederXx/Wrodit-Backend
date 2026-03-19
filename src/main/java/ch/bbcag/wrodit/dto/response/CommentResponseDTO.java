843644d4cf3bc853ffdaf21d4ee3e821016f5d9f
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-19 08:39:14 +0100

    feat(dto): create comment response dto

---
+++ src/main/java/ch/bbcag/wrodit/dto/response/CommentResponseDTO.java
@@ -0,0 +1,6 @@
+package ch.bbcag.wrodit.dto.response;
+
+import java.time.OffsetDateTime;
+
+public record CommentResponseDTO(Integer id, String content, OffsetDateTime createdAt, Integer parentId) {
+}
