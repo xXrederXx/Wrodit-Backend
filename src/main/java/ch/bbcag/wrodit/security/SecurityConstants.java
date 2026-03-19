53ba16b2db7f33c9b3d57da245729b43dd67493f
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-19 11:51:18 +0100

    feat(security): Add constants

--- src/main/java/ch/bbcag/wrodit/security/SecurityConstants.java
+++ src/main/java/ch/bbcag/wrodit/security/SecurityConstants.java
@@ -1,8 +1,24 @@
 package ch.bbcag.wrodit.security;
 
+import ch.bbcag.wrodit.controllers.AuthController;
+import java.nio.charset.StandardCharsets;
+import javax.crypto.spec.SecretKeySpec;
+
 public class SecurityConstants {
   public static final String AUTHORIZATION_HEADER_NAME = "Authorization";
 
   public static final String AUTH_HEADER_PASSWORD = "WI-Auth-Passwd";
   public static final String AUTH_HEADER_ID = "WI-Auth-Id";
+
+  public static final String SECRET = "Secret Key to generate JWT's (min 256 Bits)";
+  public static final String ALGORITHM = "HmacSHA256";
+  public static final SecretKeySpec SECRET_KEY_SPEC =
+      new SecretKeySpec(SECRET.getBytes(StandardCharsets.UTF_8), ALGORITHM);
+  ;
+  public static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 14L; // 2 Weeks
+
+  public static final String AUTH_ENDPOINTS = AuthController.PATH + "/**";
+  public static final String[] DOCS_ENDPOINTS = {
+    "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**"
+  };
 }
