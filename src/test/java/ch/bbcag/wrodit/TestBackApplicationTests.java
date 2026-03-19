06e1926f12dc20e9cc4e89ae8b82ef74d52f031b
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-19 14:32:44 +0100

    fix(test): use junit pionner to fix envvars during tests

--- src/test/java/ch/bbcag/wrodit/TestBackApplicationTests.java
+++ src/test/java/ch/bbcag/wrodit/TestBackApplicationTests.java
@@ -1,8 +1,9 @@
 package ch.bbcag.wrodit;
 
 import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.SetEnvironmentVariable;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@SetEnvironmentVariable(key="JWT_SECRET", value = "Test-JWT-Secret-ONLY-4-TESTING-PURPOSE")
 @SpringBootTest(properties = "spring.config.name=application-test")
 class TestBackApplicationTests {
 
