50df5598fdfe5f5b6a47528485d8b834290e22d5
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-19 13:20:35 +0100

    feat(jwt): add userid to the claims

--- src/main/java/ch/bbcag/wrodit/security/JWTGenerator.java
+++ src/main/java/ch/bbcag/wrodit/security/JWTGenerator.java
@@ -9,19 +9,20 @@ import com.nimbusds.jwt.SignedJWT;
 import java.util.Date;
 
 public class JWTGenerator {
-  public static String generateJwtToken(String username) {
+  public static String generateJwtToken(Integer userId, String username) {
     try {
-      SignedJWT jwt = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), buildJWTClaimsSet(username));
-      jwt.sign(new MACSigner(SecurityConstants.SECRET_KEY_SPEC));
+      SignedJWT jwt = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), buildJWTClaimsSet(userId, username));
+      jwt.sign(new MACSigner(SecurityConstants.getInstance.getSecretKeySpec()));
       return jwt.serialize();
     } catch (JOSEException e) {
       throw new RuntimeException(e);
     }
   }
 
-  private static JWTClaimsSet buildJWTClaimsSet(String username) {
+  private static JWTClaimsSet buildJWTClaimsSet(Integer userId, String username) {
     return new JWTClaimsSet.Builder()
         .subject(username)
+        .claim("userId", userId)
         .expirationTime(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
         .build();
   }
