b7dcf71f4617692f428e8bcfd5680eee3fe019b5
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-19 13:19:53 +0100

    feat(security): use env loaded key

--- src/main/java/ch/bbcag/wrodit/security/SecurityConstants.java
+++ src/main/java/ch/bbcag/wrodit/security/SecurityConstants.java
@@ -3,18 +3,26 @@ package ch.bbcag.wrodit.security;
 import ch.bbcag.wrodit.controllers.AuthController;
 import java.nio.charset.StandardCharsets;
 import javax.crypto.spec.SecretKeySpec;
+import org.springframework.beans.factory.annotation.Value;
 
 public class SecurityConstants {
+  private SecurityConstants() {
+    // hide ctor
+  }
+
+  public static final SecurityConstants getInstance = new SecurityConstants();
+
   public static final String AUTHORIZATION_HEADER_NAME = "Authorization";
 
-  public static final String AUTH_HEADER_PASSWORD = "WI-Auth-Passwd";
-  public static final String AUTH_HEADER_ID = "WI-Auth-Id";
+  @Value("${jwt.secret}")
+  private String secret;
 
-  public static final String SECRET = "Secret Key to generate JWT's (min 256 Bits)";
+  public SecretKeySpec getSecretKeySpec() {
+    return new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), ALGORITHM);
+  }
+
   public static final String ALGORITHM = "HmacSHA256";
-  public static final SecretKeySpec SECRET_KEY_SPEC =
-      new SecretKeySpec(SECRET.getBytes(StandardCharsets.UTF_8), ALGORITHM);
-  ;
+
   public static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 14L; // 2 Weeks
 
   public static final String AUTH_ENDPOINTS = AuthController.PATH + "/**";
