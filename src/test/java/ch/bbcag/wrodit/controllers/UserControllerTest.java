346f2bc5b0fa6fbbf2494844fd86fb61345d1148
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-20 10:10:58 +0100

    feat(test): add the user controller test

---
+++ src/test/java/ch/bbcag/wrodit/controllers/UserControllerTest.java
@@ -0,0 +1,88 @@
+package ch.bbcag.wrodit.controllers;
+
+import ch.bbcag.wrodit.TestingConstants;
+import ch.bbcag.wrodit.entitys.User;
+import ch.bbcag.wrodit.services.UserService;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = UserController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+
+    private static User mockUser;
+
+    @BeforeAll
+    static void init()
+    {
+        mockUser = new User();
+        mockUser.setId(1);
+        mockUser.setUsername("Tester");
+        mockUser.setEmail("test@test.com");
+        mockUser.setPasswordHash("Some-Long-Hash");
+        mockUser.setCreatedAt(OffsetDateTime.of(2026, 3, 20, 9, 13, 21, 67, ZoneOffset.UTC));
+    }
+
+    @Test
+    void checkGetById_whenValidUser_thenIsReturned() throws Exception {
+        Mockito.when(userService.findById(any(Integer.class))).thenReturn(mockUser);
+
+        mockMvc.perform(get(UserController.PATH + "/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(mockUser.getId())))
+                .andExpect(jsonPath("$.username", is(mockUser.getUsername())));
+                // .andExpect(content().string(not(containsString("\"email\"")))); Uncomment once Issue Resolves
+    }
+
+    @Test
+    @Disabled("Needs to be resolved, issue on gitlab")
+    void checkGetById_whenNoUser_then404Returned() throws Exception {
+        Mockito.when(userService.findById(any(Integer.class))).thenThrow(EntityNotFoundException.class);
+
+        mockMvc.perform(get(UserController.PATH + "/1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void checkGetAllById_whenValidUser_thenIsReturned() throws Exception {
+        Mockito.when(userService.findById(any(Integer.class))).thenReturn(mockUser);
+
+        mockMvc.perform(get(UserController.PATH + "/1/all"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(mockUser.getId())))
+                .andExpect(jsonPath("$.email", is(mockUser.getEmail())))
+                .andExpect(jsonPath("$.username", is(mockUser.getUsername())));
+    }
+
+    @Test
+    @Disabled("Needs to be resolved, issue on gitlab")
+    void checkGetAllById_whenNoUser_then404Returned() throws Exception {
+        Mockito.when(userService.findById(any(Integer.class))).thenThrow(EntityNotFoundException.class);
+
+        mockMvc.perform(get(UserController.PATH + "/1/all"))
+                .andExpect(status().isNotFound());
+    }
+}
