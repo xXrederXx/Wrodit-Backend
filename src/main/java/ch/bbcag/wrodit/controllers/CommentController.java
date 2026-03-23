4cfbe5ed999bd41d9c2b12a9340462f9e33833df
xXrederXx <morgenthalerthierry@gmail.com>
2026-03-23 12:53:39 +0100

    refactor(controller): use Injected in Comment Controller

--- src/main/java/ch/bbcag/wrodit/controllers/CommentController.java
+++ src/main/java/ch/bbcag/wrodit/controllers/CommentController.java
@@ -15,8 +15,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.net.URI;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -75,10 +74,9 @@ public class CommentController {
             description = "The user was unauthorized",
             content = @Content)
       })
-  public ResponseEntity<?> postComment(@RequestBody CommentCreateDTO commentCreateDTO) {
-    Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    Integer userId = jwt.getClaim("userId");
-
+  public ResponseEntity<?> postComment(
+      @RequestBody CommentCreateDTO commentCreateDTO,
+      @AuthenticationPrincipal(expression = "claims['userId']") Integer userId) {
     CommentResponseDTO responseDTO =
         CommentMapper.toDto(commentService.save(CommentMapper.fromDto(commentCreateDTO), userId));
     return ResponseEntity.created(URI.create(PATH + "/" + responseDTO.id())).body(responseDTO);
@@ -98,9 +96,10 @@ public class CommentController {
             content = @Content)
       })
   public ResponseEntity<?> patchComment(
-      @RequestBody CommentRequestDTO dto, @PathVariable Integer id) {
-    Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    Integer userId = jwt.getClaim("userId");
+      @RequestBody CommentRequestDTO dto,
+      @PathVariable Integer id,
+      @AuthenticationPrincipal(expression = "claims['userId']") Integer userId) {
+
     return ResponseEntity.ok(
         CommentMapper.toDto(commentService.update(CommentMapper.fromDto(dto), id, userId)));
   }
@@ -115,9 +114,10 @@ public class CommentController {
             description = "Comment was not found",
             content = @Content)
       })
-  public ResponseEntity<?> deletePost(@PathVariable Integer id) {
-    Jwt jwt = (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    Integer userId = jwt.getClaim("userId");
+  public ResponseEntity<?> deletePost(
+      @PathVariable Integer id,
+      @AuthenticationPrincipal(expression = "claims['userId']") Integer userId) {
+
     commentService.deletePostById(id, userId);
     return ResponseEntity.noContent().build();
   }
