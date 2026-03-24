7dfa696eae5dd348c949f55aceb0b530f114299b
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-24 10:38:15 +0100

    refactor(spotless): spotlessApply

--- src/main/java/ch/bbcag/wrodit/controllers/AuthController.java
+++ src/main/java/ch/bbcag/wrodit/controllers/AuthController.java
@@ -15,7 +15,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import java.net.URI;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -26,7 +25,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(AuthController.PATH)
