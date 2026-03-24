06565aa0c529be83e6a531afb66bb2d778980f5d
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-24 11:14:02 +0100

    refactor(spotless): spotlessApply

--- src/main/java/ch/bbcag/wrodit/dto/response/UserResponseDTO.java
+++ src/main/java/ch/bbcag/wrodit/dto/response/UserResponseDTO.java
@@ -1,6 +1,6 @@
 package ch.bbcag.wrodit.dto.response;
 
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
-public record UserResponseDTO(Integer id, String username, String email, OffsetDateTime createdAt) {}
+public record UserResponseDTO(
+    Integer id, String username, String email, OffsetDateTime createdAt) {}
