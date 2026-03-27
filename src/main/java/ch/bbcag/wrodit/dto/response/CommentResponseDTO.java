6ce805bce3d3ab9cf061c742c1306b6d3c57d745
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-27 08:51:47 +0100

    refactor(spotless): spotlessApply

--- src/main/java/ch/bbcag/wrodit/dto/response/CommentResponseDTO.java
+++ src/main/java/ch/bbcag/wrodit/dto/response/CommentResponseDTO.java
@@ -4,13 +4,9 @@ import java.time.OffsetDateTime;
 
 public record CommentResponseDTO(
     Integer id,
-   
     String content,
-   
     Integer votes,
-   
     OffsetDateTime createdAt,
-   
     Integer parentId,
     Integer postId,
     Integer userId) {}
