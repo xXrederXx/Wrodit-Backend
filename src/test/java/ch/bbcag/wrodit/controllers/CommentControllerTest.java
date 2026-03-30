435354cb4d008eae4df0a51cb2cc7878ea9b3dcc
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-30 10:39:34 +0200

    feat(test): add tests

--- src/test/java/ch/bbcag/wrodit/controllers/CommentControllerTest.java
+++ src/test/java/ch/bbcag/wrodit/controllers/CommentControllerTest.java
@@ -13,6 +13,7 @@ import ch.bbcag.wrodit.entities.Comment;
 import ch.bbcag.wrodit.services.CommentService;
 import ch.bbcag.wrodit.services.CommentVoteService;
 import ch.bbcag.wrodit.util.URIHelper;
+import ch.bbcag.wrodit.util.exception.FailedValidationException;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.Arrays;
 import java.util.List;
@@ -25,6 +26,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -46,6 +48,7 @@ class CommentControllerTest {
     mockComment = comments[0];
   }
 
+  // Get By Id
   @Test
   void checkGetById_whenValidComment_thenIsReturned() throws Exception {
     Mockito.when(commentService.getCommentById(any(Integer.class))).thenReturn(mockComment);
@@ -67,6 +70,7 @@ class CommentControllerTest {
         .andExpect(status().isNotFound());
   }
 
+  // Get Comments
   @Test
   void checkGetComments_whenCommentExists_thenSuccess() throws Exception {
     Mockito.when(commentService.getPaginatedComments(any(Pageable.class), any(), any()))
@@ -83,6 +87,7 @@ class CommentControllerTest {
     mockMvc.perform(get(URIHelper.join(CommentController.PATH, ""))).andExpect(status().isOk());
   }
 
+  // Post Comments
   @Test
   void checkPostComments_whenValidComment_thenCreated() throws Exception {
     Mockito.when(commentService.save(any(Comment.class), any())).thenReturn(mockComment);
@@ -120,6 +125,40 @@ class CommentControllerTest {
   }
 
   @Test
+  void checkPostComments_whenEmptyComment_thenBadRequest() throws Exception {
+    mockMvc
+        .perform(
+            post(URIHelper.join(CommentController.PATH, ""))
+                .contentType(TestingUtil.CONTENT_TYPE_JSON)
+                .content(
+                    """
+                                                        {
+                                                            "content":" "
+                                                        }
+                                                        """))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void checkPostComments_whenInvalidPostId_thenNotFound() throws Exception {
+    Mockito.when(commentService.save(any(Comment.class), any()))
+        .thenThrow(EntityNotFoundException.class);
+    mockMvc
+        .perform(
+            post(URIHelper.join(CommentController.PATH, ""))
+                .contentType(TestingUtil.CONTENT_TYPE_JSON)
+                .content(
+                    """
+                                            {
+                                              "content": "string",
+                                              "postId": 0
+                                            }
+                                            """))
+        .andExpect(status().isNotFound());
+  }
+
+  // Patch Comment
+  @Test
   void checkPatchComment_whenValidComment_thenOk() throws Exception {
     Mockito.when(commentService.update(any(Comment.class), any(), any())).thenReturn(mockComment);
 
@@ -136,6 +175,43 @@ class CommentControllerTest {
         .andExpect(status().isOk());
   }
 
+  @Test
+  void checkPatchComment_whenInvalidComment_thenOk() throws Exception {
+    Mockito.when(commentService.update(any(Comment.class), any(), any()))
+        .thenThrow(FailedValidationException.class);
+
+    mockMvc
+        .perform(
+            patch(URIHelper.join(CommentController.PATH, "1"))
+                .contentType(TestingUtil.CONTENT_TYPE_JSON)
+                .content(
+                    """
+                                                                {
+                                                                  "content":" "
+                                                                }
+                                                        """))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  void checkPatchComment_whenForbidden_thenFails() throws Exception {
+    Mockito.when(commentService.update(any(Comment.class), any(), any()))
+        .thenThrow(AccessDeniedException.class);
+
+    mockMvc
+        .perform(
+            patch(URIHelper.join(CommentController.PATH, "1"))
+                .contentType(TestingUtil.CONTENT_TYPE_JSON)
+                .content(
+                    """
+                                                                {
+                                                                  "content":"idk"
+                                                                }
+                                                        """))
+        .andExpect(status().isForbidden());
+  }
+
+  // Delete Comment
   @Test
   void checkDeleteComment_whenValidId_thenNoContent() throws Exception {
     Mockito.doNothing().when(commentService).deletePostById(any(), any());
@@ -154,6 +230,7 @@ class CommentControllerTest {
         .andExpect(status().isNotFound());
   }
 
+  // Put Vote
   @Test
   void checkVote_whenValidId_thenOk() throws Exception {
     Mockito.when(commentVoteService.update(anyInt(), anyInt(), anyInt()))
@@ -192,6 +269,21 @@ class CommentControllerTest {
   }
 
   @Test
+  void checkVote_whenInvalidVote_thenBadRequest() throws Exception {
+    mockMvc
+        .perform(
+            put(URIHelper.join(CommentController.PATH, "1/vote"))
+                .contentType(TestingUtil.CONTENT_TYPE_JSON)
+                .content(
+                    """
+                                            {
+                                              "vote":10
+                                            }
+                                            """))
+        .andExpect(status().isBadRequest());
+  }
+
+  @Test
   void checkVote_whenInvalidVote_thenNotFound() throws Exception {
     mockMvc
         .perform(
@@ -206,6 +298,7 @@ class CommentControllerTest {
         .andExpect(status().isBadRequest());
   }
 
+  // Delete Vote
   @Test
   void checkDeleteVote_whenValidVote_thenOk() throws Exception {
     Mockito.doNothing().when(commentVoteService).deleteById(any(), any());
@@ -225,5 +318,14 @@ class CommentControllerTest {
     mockMvc
         .perform(delete(URIHelper.join(CommentController.PATH, "1/vote")))
         .andExpect(status().isNotFound());
+  }
+
+  @Test
+  void checkDeleteVote_whenForbidden_thenFails() throws Exception {
+    Mockito.doThrow(AccessDeniedException.class).when(commentVoteService).deleteById(any(), any());
+
+    mockMvc
+        .perform(delete(URIHelper.join(CommentController.PATH, "1/vote")))
+        .andExpect(status().isForbidden());
   }
 }
