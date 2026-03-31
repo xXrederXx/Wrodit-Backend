7c6317e8088a754fd9d81bd9beae4bf1d4d8b3c1
Thierry Morgenthaler <thierry.morgenthaler@bbcag.ch>
2026-03-31 10:07:42 +0200

    feat(test): add tests

--- src/test/java/ch/bbcag/wrodit/GlobalExeptionHandlerTest.java
+++ src/test/java/ch/bbcag/wrodit/GlobalExeptionHandlerTest.java
@@ -1,9 +1,12 @@
 package ch.bbcag.wrodit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import ch.bbcag.wrodit.util.exception.FailedValidationException;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -12,57 +15,65 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 class GlobalExceptionHandlerTest {
-    private static GlobalExceptionHandler handler;
+  private static GlobalExceptionHandler handler;
 
-    @BeforeAll
-    static void init() {
-        handler = new GlobalExceptionHandler();
-    }
+  @BeforeAll
+  static void init() {
+    handler = new GlobalExceptionHandler();
+  }
 
-    @Test
-    void checkAllExceptions_returns500() {
-        ResponseEntity<?> res = handler.handleAllExceptions(new Exception());
-        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, res.getStatusCode());
-    }
+  @Test
+  void checkAllExceptions_returns500() {
+    ResponseEntity<?> res = handler.handleAllExceptions(new Exception());
+    assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, res.getStatusCode());
+  }
 
-    @Test
-    void checkAuthorizationDeniedException_returns401() {
-        ResponseEntity<?> res =
-                handler.handleAuthorizationDeniedException(new AuthorizationDeniedException(""));
-        assertEquals(HttpStatus.UNAUTHORIZED, res.getStatusCode());
-    }
+  @Test
+  void checkAuthorizationDeniedException_returns401() {
+    ResponseEntity<?> res =
+        handler.handleAuthorizationDeniedException(new AuthorizationDeniedException(""));
+    assertEquals(HttpStatus.UNAUTHORIZED, res.getStatusCode());
+  }
 
-    @Test
-    void checkMethodArgumentNotValidException_returns400() {
-        ResponseEntity<?> res = handler.handleAllExceptions(new Exception());
-        assertEquals(HttpStatus.BAD_REQUEST, res.getStatusCode());
-    }
+  @Test
+  void checkMethodArgumentNotValidException_returns400() {
+    MethodArgumentNotValidException ex = mock(MethodArgumentNotValidException.class);
+    BindingResult bindingResult = mock(BindingResult.class);
+    when(ex.getBindingResult()).thenReturn(bindingResult);
+    when(bindingResult.getAllErrors()).thenReturn(new ArrayList<>());
 
-    @Test
-    void checkDataIntegrityViolationException_returns409() {
-        ResponseEntity<?> res =
-                handler.handleDataIntegrityViolationException(new DataIntegrityViolationException(""));
-        assertEquals(HttpStatus.CONFLICT, res.getStatusCode());
-    }
+    ResponseEntity<?> res = handler.handleMethodArgumentNotValidException(ex);
+    assertEquals(HttpStatus.BAD_REQUEST, res.getStatusCode());
+  }
 
-    @Test
-    void checkAccessDeniedException_returns403() {
-        ResponseEntity<?> res = handler.handleAllExceptions(new AccessDeniedException("Forbidden"));
-        assertEquals(HttpStatus.FORBIDDEN, res.getStatusCode());
-    }
+  @Test
+  void checkDataIntegrityViolationException_returns409() {
+    ResponseEntity<?> res =
+        handler.handleDataIntegrityViolationException(new DataIntegrityViolationException(""));
+    assertEquals(HttpStatus.CONFLICT, res.getStatusCode());
+  }
 
-    @Test
-    void checkFailedValidationException_returns400() {
-        ResponseEntity<?> res =
-                handler.handleAllExceptions(new FailedValidationException(new HashMap<>(10)));
-        assertEquals(HttpStatus.BAD_REQUEST, res.getStatusCode());
-    }
+  @Test
+  void checkAccessDeniedException_returns403() {
+    ResponseEntity<?> res =
+        handler.handleAccessDeniedException(new AccessDeniedException("Forbidden"));
+    assertEquals(HttpStatus.FORBIDDEN, res.getStatusCode());
+  }
 
-    @Test
-    void checkEntityNotFoundException_returns404() {
-        ResponseEntity<?> res = handler.handleAllExceptions(new EntityNotFoundException());
-        assertEquals(HttpStatus.NOT_FOUND, res.getStatusCode());
-    }
+  @Test
+  void checkFailedValidationException_returns400() {
+    ResponseEntity<?> res =
+        handler.handleFailedValidationException(new FailedValidationException(new HashMap<>(10)));
+    assertEquals(HttpStatus.BAD_REQUEST, res.getStatusCode());
+  }
+
+  @Test
+  void checkEntityNotFoundException_returns404() {
+    ResponseEntity<?> res = handler.handleEntityNotFoundException(new EntityNotFoundException());
+    assertEquals(HttpStatus.NOT_FOUND, res.getStatusCode());
+  }
 }
